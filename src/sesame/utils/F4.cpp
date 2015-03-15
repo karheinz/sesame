376b8de22080a2fe8a80858c5572c1562b3a2124
Karsten Heinze <karsten@sidenotes.de>
2015-03-15 23:56:12 +0100

    added libjpeg and f4 sources

---
+++ src/sesame/utils/F4.cpp
@@ -0,0 +1,20 @@
+#include "sesame/utils/F4.hpp"
+#include "sesame/utils/jpegtranf4.h"
+
+
+namespace sesame { namespace utils {
+
+F4::F4( const String& fileName ) :
+   m_FileName( fileName )
+{
+}
+
+void F4::embed( std::istream& s )
+{
+}
+
+void F4::extract( std::ostream& s )
+{
+}
+
+} }
